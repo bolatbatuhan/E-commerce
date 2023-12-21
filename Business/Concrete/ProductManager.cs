7b60b5cca7d739f5902509589862f28d7528ecb6
Batuhan Bolat <bolatbatuhan@gmail.com>
2023-12-21 15:38:01 +0300

    ProductController, IoC added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -30,10 +30,10 @@ public class ProductManager : IProductService
 
     public IDataResult<List<Product>> GetAll()
     {
-        if(DateTime.Now.Hour == 22)
-        {
-            return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-        }
+        //if(DateTime.Now.Hour == 15)
+        //{
+        //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+        //}
 
         return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed);
     }
